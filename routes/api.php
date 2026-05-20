70a8b14e57d537fc83b50ccf91f8c3c6bd206288
Chris <playschris232@gmail.com>
2026-05-21 00:13:23 +0700

    feat: menyelesaikan backend api dan frontend modern modul obat serta postman sukses

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\ObatController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Kode bawaan Laravel (Biarkan saja, tidak usah dihapus)
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// 2. Tambahkan rute Modul Obat milikmu di bawah sini
+Route::apiResource('obats', ObatController::class);
